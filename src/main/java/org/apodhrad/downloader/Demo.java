746098d0f4d64fe8355e56876dd8752e97a66089
Andrej Podhradsky <apodhrad@redhat.com>
2014-08-15 17:50:26 +0200

    Initial CLI implementation

--- src/main/java/org/apodhrad/downloader/Demo.java
+++ src/main/java/org/apodhrad/downloader/Demo.java
@@ -1,8 +1,13 @@
 package org.apodhrad.downloader;
 
+/**
+ * 
+ * @author apodhrad
+ *
+ */
 public class Demo {
 
-	public static void main(String[] args) {
-		System.out.println("Hello World");
+	public static void main(String[] args) throws Exception {
+		System.out.println("Downloader: CLI not supported");
 	}
 }
